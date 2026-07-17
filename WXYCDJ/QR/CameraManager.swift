2f40c14b2a238d7dde1c418fa5c4ba00b727b16a
Meira Volk <meiravolk@gmail.com>
2026-07-17 17:27:53 -0400

    Added camera functionality & qr code scanner, saves output as string. Added new toolbar to MainView.

---
+++ WXYCDJ/QR/CameraManager.swift
@@ -0,0 +1,114 @@
+//
+//  CameraManager.swift
+//  WXYCDJ
+//
+//  Created by Meira Volk on 7/17/26.
+//
+import AVFoundation
+import SwiftUI
+import Combine
+
+class CameraManager: NSObject, ObservableObject, AVCaptureMetadataOutputObjectsDelegate{
+    @Published var capturedCode: String?
+    @Published var isSessionRunning = false
+    @Published var authorizationStatus: AVAuthorizationStatus = .notDetermined
+    //AVFoundation Components
+    let session = AVCaptureSession()
+    private let metadataObjectsQueue = DispatchQueue(label: "metadata objects queue", attributes: [], target: nil)
+    let qrOutput = AVCaptureMetadataOutput()
+    
+    private var currentInput: AVCaptureDeviceInput?
+    
+    private let sessionQueue = DispatchQueue(label: "com.customcamera.sesssionQueue")
+    
+    override init() {
+        super.init()
+    }
+    func checkAuthorization () {
+        switch AVCaptureDevice.authorizationStatus(for: .video){
+        case .authorized:
+            authorizationStatus = .authorized
+            setupSession()
+            
+        case .notDetermined:
+            authorizationStatus = .notDetermined
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                DispatchQueue.main.async {
+                    self?.authorizationStatus = granted ? .authorized : .denied
+                    if granted {
+                        self?.setupSession()
+                    }
+                }
+                
+            }
+        case .denied, .restricted:
+            authorizationStatus = .denied
+        @unknown default:
+            authorizationStatus = .denied
+        }
+    }
+    //Config AVSetup
+    private func setupSession() {
+        sessionQueue.async {
+            [weak self] in
+            guard let self = self else {return}
+            
+            //set session preset
+            self.session.beginConfiguration()
+            self.session.sessionPreset = .high
+            
+            // Set delegate and use the default dispatch queue to execute the call back
+            //camera input
+            
+            guard let camera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back), let input = try? AVCaptureDeviceInput(device: camera) else {
+                print("Failed to access camera")
+                self.session.commitConfiguration()
+                return
+            }
+            if self.session.canAddInput(input){
+                self.session.addInput(input)
+                self.currentInput = input
+            }
+            // add qr output
+            
+            if self.session.canAddOutput(self.qrOutput){
+                self.session.addOutput(self.qrOutput)
+                //May need to add more here from appcoda
+                qrOutput.setMetadataObjectsDelegate(self, queue: metadataObjectsQueue)
+                qrOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            }
+            self.session.commitConfiguration()
+            
+            //start the session
+            
+            self.session.startRunning()
+                       
+            DispatchQueue.main.async {
+                self.isSessionRunning = self.session.isRunning
+            }
+        }
+    }
+    
+       nonisolated func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection){
+        
+        Task { @MainActor in
+            // Checks metadataObjects array is not empty
+            
+            if metadataObjects.count == 0 {
+                self.capturedCode = "No QR code is detected"
+                return
+            }
+            // Safely unwrap the metadata object
+            guard let metadataObj = metadataObjects[0] as? AVMetadataMachineReadableCodeObject else { return }
+           
+                if metadataObj.type == .qr {
+                    // If a string value exists, update your @Published property
+                    if let metadataString = metadataObj.stringValue {
+                        self.capturedCode = metadataString
+                    }
+                }
+            
+        }
+    }
+}
+
---
+++ WXYCDJ/QR/CameraView.swift
@@ -0,0 +1,64 @@
+//
+//  CameraView.swift
+//  WXYCDJ
+//
+//  Created by Meira Volk on 7/11/26.
+//
+
+
+import SwiftUI
+import AVFoundation
+import AVKit //for photo quality I think
+
+struct CameraView: View {
+
+    @Binding var showScanner: Bool
+    @Binding var qrCode: String
+    @StateObject private var cameraManager = CameraManager()
+    let onDismiss: () -> Void
+    
+    var body: some View {
+        //ZStack for layering preview w/ controls
+        ZStack {
+            if cameraManager.authorizationStatus == .authorized {
+                CameraPreview(session: cameraManager.session, qrOutput: cameraManager.qrOutput) .ignoresSafeArea()
+            } else {
+                VStack{
+                    Image(systemName: "camera.fill")
+                        .font(.largeTitle)
+                        .foregroundStyle(.gray)
+                    Text("Camera Access Required")
+                        .font(.largeTitle)
+                        .foregroundStyle(.gray)
+                    
+                    if cameraManager.authorizationStatus == .denied{
+                        Text("please enable camera in settings")
+                        
+                        Button("Open Settings"){
+                            if let settingsURL = URL(string:
+                                                        UIApplication.openSettingsURLString){
+                                UIApplication.shared.open(settingsURL)
+                            }
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                }
+            }
+            }
+        .onAppear {
+            cameraManager.checkAuthorization()
+        }
+        .onChange(of: cameraManager.capturedCode) { oldCode, newCode in
+            if let metadataString = cameraManager.capturedCode {
+                if metadataString != "No QR code is detected" {
+                    qrCode = metadataString
+                    onDismiss()
+                }
+            }
+        }
+        .padding()
+        }
+        
+        
+     
+}
